62a3a13e1b0865c1563d9ab867954c72a2e1ad4a
benh42 <56282575+benh42@users.noreply.github.com>
2020-07-18 14:50:00 -0700

    Cookies and Session
    saves data between requests

--- optimus_time.py
+++ optimus_time.py
@@ -1,6 +1,15 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, make_response,session,jsonify
 from markupsafe import escape
 app = Flask(__name__) 
+app.secret_key = b'O*D:&fO#O&OPS;{Q:l'
+
+class Work():
+    def __init__(self,name,dur,due):
+        self.name = name
+        self.dur = dur
+        self.due = due
+    def __repr__(self):
+        return f"{self.name} : {self.dur} : {self.due}"
 
 def valid_login(username,password):
     return False
@@ -11,23 +20,24 @@ def log_the_user_in(username):
 def index():
     return 'index'
 
-@app.route('/optimus-time', methods=['POST', 'GET'])
+@app.route('/session-info', methods=['POST', 'GET'])
 def login():
-    error = None
+    name=None
+    dur=None
+    due=None
+
+    session["test"] = [1,2]
+
     if request.method == 'POST':
         name = request.form['name']
         dur = request.form['duration_hr']
         due = request.form['due_in_x']
-        
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('optimus_time.html', error=error)
+        session[f"{name}"] = [dur,due]
+    if 'all_works' in session:
+        session.pop('all_works',None)
 
 
-    list_of_assignments = request.form['assingments']
-    #"do algorithm on list"
+    return jsonify([session[n] for n in session])
 
 with app.test_request_context():
     print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
